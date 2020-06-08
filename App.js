99931d5488f8983387656f740505ef4dd7f8e528
roberto cordero <mago_ro_96@hotmail.com>
2020-06-07 20:19:59 -0700

    primer commiT

--- App.js
+++ App.js
@@ -1,19 +1,93 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import axios from 'axios';
+import SelectInput from 'react-native-select-input-ios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component
+{ 
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      loading: true,
+      tableHead: [],
+      tableData: [],
+      options: [{ value: "mexico", label: 'Mexico'},
+                {value: "italy", label: "Italia"},
+                {value: "Spain", label: "España"}]
+    }
+  }
+
+  componentDidMount()
+  {
+    this.load("mexico");
+  }
+
+  changeCountry(value)
+  {
+    this.load(value);
+  }
+
+  load(pais)
+  {
+      return axios({
+        "method":"GET",
+        "url":"https://covid-19-data.p.rapidapi.com/country",
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key":"e36277b228msh7650bcd4f87129dp18296ajsn2c9a98a8b9d1",
+        "useQueryString":true
+      },"params":{
+        "format":"json",
+        "name":pais,
+      }
+    })
+    .then((response)=>{
+      let res = response["data"][0];
+      let array = [res["code"],res["country"],res["confirmed"],res["recovered"],res["deaths"]];
+      this.setState({
+        loading:false,
+        tableHead: ['codigo','pais','confirmados','recuperados','muertos'],
+        tableData: [
+          array,
+          ['codigo','pais','confirmados','recuperados','muertos']
+        ]
+      })
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  render()
+  { 
+    if(this.state.loading == false)
+    {
+      const state = this.state;
+      return (
+        <View style={styles.container}>
+           <SelectInput style={{margin:50}} value={0} options={state.options} onSubmitEditing={this.changeCountry.bind(this)} />
+          <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff',width: 200}}>
+            <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+            <Rows data={state.tableData} textStyle={styles.text}/>
+          </Table>
+        </View>
+        );
+    }
+    else
+    {
+      return (
+        <ActivityIndicator></ActivityIndicator>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#c3c3c3' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
 });
+
